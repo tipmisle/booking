2cad317d473088de6cb6e609443b882634436bf3
Josip Peder Rajković <pedro@josip.si>
2013-10-28 00:58:42 +0100

    no message

--- footer.php
+++ footer.php
@@ -1,2 +1,4 @@
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="js/boostrap.min.js">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<title>neki</title>
 </head>
