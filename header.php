a79b63d83587fcae4623d76fffac213b50ae9518
Miha Vidakovic <miha@vidakovic.si>
2013-10-28 00:47:14 +0100

    Main upload

---
+++ header.php
@@ -0,0 +1,7 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="js/boostrap.min.js">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<title>neki</title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,74 @@
+<?php include 'header.php'; ?>
+<TABLE class="btn-group" data-toggle="buttons"> 
+		<TR>
+		<TD COLSPAN="7" class="koledar"><B>December 2001</B></TD> 
+		</TR>
+		<TR> 
+		<TD COLSPAN="7" class="koledar"><I>Another year comes to an end</I></TD>
+		</TR>
+		<TR> 
+				<TD>Sun</TD>
+				<TD>Mon</TD>
+				<TD>Tue</TD>
+				<TD>Wed</TD>
+				<TD>Thu</TD>
+				<TD>Fri</TD>
+				<TD>Sat</TD>
+		</TR>
+		<TR> 
+				<TD class="btn btn-primary"></TD>
+				<TD class="btn btn-primary"></TD>
+				<TD class="btn btn-primary"></TD>
+				<TD class="btn btn-primary"></TD>
+				<TD class="btn btn-primary"></TD>
+				<TD class="btn btn-primary"></TD>
+				<TD class="btn btn-primary"><input type="checkbox">1</TD>
+		</TR>
+		<TR> 
+				<TD class="btn btn-primary"><input type="checkbox">2</TD>
+				<TD class="btn btn-primary"><input type="checkbox">3</TD>
+				<TD class="btn btn-primary"><input type="checkbox">4</TD>
+				<TD class="btn btn-primary"><input type="checkbox">5</TD>
+				<TD class="btn btn-primary"><input type="checkbox">6</TD>
+				<TD class="btn btn-primary"><input type="checkbox">7</TD>
+				<TD class="btn btn-primary"><input type="checkbox">8</TD>
+		</TR>
+		<TR> 
+				<TD class="btn btn-primary"><input type="checkbox">9</TD>
+				<TD class="btn btn-primary"><input type="checkbox">10</TD>
+				<TD class="btn btn-primary"><input type="checkbox">11</TD>
+				<TD class="btn btn-primary"><input type="checkbox">12</TD>
+				<TD class="btn btn-primary"><input type="checkbox">13</TD>
+				<TD class="btn btn-primary"><input type="checkbox">14</TD>
+				<TD class="btn btn-primary"><input type="checkbox">15</TD>
+		</TR>
+		<TR> 
+				<TD class="btn btn-primary"><input type="checkbox">16</TD>
+				<TD class="btn btn-primary"><input type="checkbox">17</TD>
+				<TD class="btn btn-primary"><input type="checkbox">18</TD>
+				<TD class="btn btn-primary"><input type="checkbox">19</TD>
+				<TD class="btn btn-primary"><input type="checkbox">20</TD>
+				<TD class="btn btn-primary"><input type="checkbox">21</TD>
+				<TD class="btn btn-primary"><input type="checkbox">22</TD>
+		</TR>
+		<TR> 
+				<TD class="btn btn-primary"><input type="checkbox">23</TD>
+				<TD class="btn btn-primary"><input type="checkbox">24</TD>
+				<TD class="btn btn-primary"><input type="checkbox">25</TD>
+				<TD class="btn btn-primary"><input type="checkbox">26</TD>
+				<TD class="btn btn-primary"><input type="checkbox">27</TD>
+				<TD class="btn btn-primary"><input type="checkbox">28</TD>
+				<TD class="btn btn-primary"><input type="checkbox">29</TD>
+		</TR>
+		<TR> 
+				<TD class="btn btn-primary"><input type="checkbox">30</TD>
+				<TD class="btn btn-primary"><input type="checkbox">31</TD>
+				<TD class="btn btn-primary"></TD>
+				<TD class="btn btn-primary"></TD>
+				<TD class="btn btn-primary"></TD>
+				<TD class="btn btn-primary"></TD>
+				<TD class="btn btn-primary"></TD>
+
+		</TR>
+		</TABLE>
+<?php include 'footer.php'; ?>
