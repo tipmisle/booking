35bcc3d5ffda1c401ab7b511a291044535255a51
Miha Vidaskovič <miha@vidakovic.si>
2013-10-28 01:30:08 +0100

    no message

--- footer.php
+++ footer.php
@@ -1,4 +1,30 @@
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="components/jquery/jquery.min.js"></script>
+	<script type="text/javascript" src="components/underscore/underscore-min.js"></script>
+	<script type="text/javascript" src="components/bootstrap3/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="components/jstimezonedetect/jstz.min.js"></script>
+	<script type="text/javascript" src="js/language/fr-FR.js"></script>
+	<script type="text/javascript" src="js/language/de-DE.js"></script>
+	<script type="text/javascript" src="js/language/el-GR.js"></script>
+	<script type="text/javascript" src="js/language/it-IT.js"></script>
+	<script type="text/javascript" src="js/language/pl-PL.js"></script>
+	<script type="text/javascript" src="js/language/pt-BR.js"></script>
+	<script type="text/javascript" src="js/language/es-MX.js"></script>
+	<script type="text/javascript" src="js/language/es-ES.js"></script>
+	<script type="text/javascript" src="js/language/ru-RU.js"></script>
+	<script type="text/javascript" src="js/language/sv-SE.js"></script>
+	<script type="text/javascript" src="js/calendar.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
+
+	<script type="text/javascript">
+		var disqus_shortname = 'bootstrapcalendar'; // required: replace example with your forum shortname
+		(function() {
+			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+			dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+		})();
+	</script>
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/calendar.css">
 	<title>neki</title>
 </head>
 <body>
